049f8a800f579288ed4730fc1d7dd8746d50d11d
yjain9301 <yashjain@softopstechnologies.com>
2024-03-27 16:07:18 +0530

    2nd

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/Apdcvbnm,ap.tsx</code> and save to reload.
+          Edit  and save to reload.
         </p>
         Learn React with mateial ui
       </header>
